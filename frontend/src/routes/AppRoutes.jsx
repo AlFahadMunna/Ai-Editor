3e49e3d23d2b4d8f9b6e219ceee9db2f3ef589a6
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-01-17 22:13:28 +0600

    login page created

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../screens/Login";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>Home</div>} />
-        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<div>Register</div>} />
       </Routes>
     </BrowserRouter>
