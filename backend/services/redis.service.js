58cc24438848507e2df0e3c9b557341c9f329d64
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-01-17 21:27:13 +0600

    redis setup

---
+++ backend/services/redis.service.js
@@ -0,0 +1,13 @@
+import Redis from "ioredis";
+
+const redisCLient = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
+
+redisCLient.on("connect", () => {
+  console.log("Redis connected");
+});
+
+export default redisCLient;
