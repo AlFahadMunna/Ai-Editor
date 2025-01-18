9d10e2c97de70ee03f07723927ae762385e472b8
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-01-18 18:52:35 +0600

    password ignored for frontend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -7,11 +7,12 @@ export const createUserController = async (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: error.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
   try {
     const user = await userService.createUser(req.body);
     const token = await user.generateJWT();
+    delete user._doc.password;
     res.status(201).json({ user, token });
   } catch (error) {
     res.status(400).send(error.message);
