73d824ad532b10b7bd72f1292a4a10a112b91add
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-01-19 20:13:22 +0600

    project schema added

---
+++ backend/models/project.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+});
