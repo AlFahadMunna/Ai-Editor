fdb4d1f3df00823eb3f8cb7953da9e1c90aa93c9
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-01-21 23:09:34 +0600

    headeer added

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -4,7 +4,17 @@ import { useLocation } from "react-router-dom";
 const Project = ({ navigate }) => {
   const location = useLocation();
   console.log(location.state);
-  return <div>Project</div>;
+  return (
+    <main className="h-screen w-screen flex">
+      <section className="left h-full min-w-60 bg-red-50">
+        <header className="flex justify-end  p-2 px-4 w-full bg-slate-200">
+          <button className="p-2">
+            <i className="ri-group-fill"></i>
+          </button>
+        </header>
+      </section>
+    </main>
+  );
 };
 
 export default Project;
